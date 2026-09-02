afd69d5477faef627bf55dd0b46e79ab17c16a5b
Jannie NGOY ITUKA <janniengoyituka@icloud.com>
2026-09-02 11:44:41 +0200

    7 . exercice

--- mini-crm/routes/web.php
+++ mini-crm/routes/web.php
@@ -10,6 +10,15 @@ Route::get('/bonjour', function () {
     return '<h1>Bienvenue dans mon Mini-CRM !</h1>';
 });
 
+Route::get('/bonjour/{nom}', function ($nom) {
+    return "Bonjour $nom !";
+});
+
 Route::get('/contact', function () {
     return 'Contactez-nous';
+});
+
+//7. exercice
+Route::get('/produit/{nom}', function ($nom) {
+    return "Vous consultez le produit : $nom.";
 });
