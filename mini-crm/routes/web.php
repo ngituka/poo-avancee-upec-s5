602db3dd954691450c313f3ac9603be0caad4817
Jannie NGOY ITUKA <janniengoyituka@icloud.com>
2026-09-02 11:37:32 +0200

    5. modifier la réponse

--- mini-crm/routes/web.php
+++ mini-crm/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/bonjour', function () {
+    return '<h1>Bienvenue dans mon Mini-CRM !</h1>';
+});
+
+Route::get('/contact', function () {
+    return 'Contactez-nous';
+});
